90ff11776b8e6e6c066aff069a2853cb1bab493f
exfarnanda1945 <exfarnanda1945@gmail.com>
2022-11-27 13:05:10 +0700

    add: 3 main fragment logic

---
+++ app/src/main/java/com/exfarnanda1945/my_foody_course/FavoriteRecipesFragment.kt
@@ -0,0 +1,18 @@
+package com.exfarnanda1945.my_foody_course
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FavoriteRecipesFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/exfarnanda1945/my_foody_course/RecipesFragment.kt
@@ -0,0 +1,32 @@
+package com.exfarnanda1945.my_foody_course
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.exfarnanda1945.my_foody_course.databinding.FragmentRecipesBinding
+
+
+class RecipesFragment : Fragment() {
+    private var _binding: FragmentRecipesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.recipesRecyclerview.showShimmer()
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
