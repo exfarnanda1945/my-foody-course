ebd826df67d38a18cd59307e27d89ef6547ef69b
exfarnanda1945 <exfarnanda1945@gmail.com>
2022-12-06 20:48:52 +0700

    add: more constant variable

--- app/src/main/java/com/exfarnanda1945/my_foody_course/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.exfarnanda1945.my_foody_course
-
-object Constants {
-    const val API_KEY ="eae6ab421f554d2a8f5ff823ec9467ea"
-    const val BASE_URL ="https://api.spoonacular.com"
-}
---
+++ app/src/main/java/com/exfarnanda1945/my_foody_course/util/Constants.kt
@@ -0,0 +1,14 @@
+package com.exfarnanda1945.my_foody_course.util
+
+object Constants {
+    const val API_KEY ="eae6ab421f554d2a8f5ff823ec9467ea"
+    const val BASE_URL ="https://api.spoonacular.com"
+
+
+    const val QUERY_NUMBER = "number"
+    const val QUERY_API_KEY = "apiKey"
+    const val QUERY_TYPE = "type"
+    const val QUERY_DIET = "diet"
+    const val QUERY_ADD_RECIPE_INFO = "addRecipeInformation"
+    const val QUERY_FILL_INGREDIENTS = "fillIngredients"
+}
