36b9a6bc94edbe13c255e41436e9e664ac444579
exfarnanda1945 <exfarnanda1945@gmail.com>
2022-12-07 21:56:49 +0700

    add: Recipes entity

---
+++ app/src/main/java/com/exfarnanda1945/my_foody_course/data/database/RecipesEntity.kt
@@ -0,0 +1,17 @@
+package com.exfarnanda1945.my_foody_course.data.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.exfarnanda1945.my_foody_course.model.FoodRecipe
+import com.exfarnanda1945.my_foody_course.util.Constants.RECIPES_TABLE_NAME
+
+
+// Store json object into room
+@Entity(tableName = RECIPES_TABLE_NAME)
+class RecipesEntity(
+    var foodRecipe: FoodRecipe
+) {
+    // when fetch a new data from api, the data in room will replaced all
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = 0
+}
