230334e6e82fb824234c6ff5ef440e9df041642c
exfarnanda1945 <exfarnanda1945@gmail.com>
2022-12-07 21:57:14 +0700

    add: Recipes DAO

---
+++ app/src/main/java/com/exfarnanda1945/my_foody_course/data/database/RecipesDao.kt
@@ -0,0 +1,17 @@
+package com.exfarnanda1945.my_foody_course.data.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RecipesDao {
+    // replace data in database when fetch from api
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipes(recipesEntity: RecipesEntity)
+
+    @Query("SELECT * FROM recipes_table ORDER BY id ASC")
+    fun readRecipes():Flow<List<RecipesEntity>>
+}
