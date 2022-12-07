408997678aa276fdbfa99c1d22e157e7ed8f9d28
exfarnanda1945 <exfarnanda1945@gmail.com>
2022-12-07 21:58:18 +0700

    add: Constant variable for database

--- app/src/main/java/com/exfarnanda1945/my_foody_course/util/Constants.kt
+++ app/src/main/java/com/exfarnanda1945/my_foody_course/util/Constants.kt
@@ -1,14 +1,20 @@
 package com.exfarnanda1945.my_foody_course.util
 
 object Constants {
+    // API
     const val API_KEY ="eae6ab421f554d2a8f5ff823ec9467ea"
     const val BASE_URL ="https://api.spoonacular.com"
 
-
+    // Query Api
     const val QUERY_NUMBER = "number"
     const val QUERY_API_KEY = "apiKey"
     const val QUERY_TYPE = "type"
     const val QUERY_DIET = "diet"
     const val QUERY_ADD_RECIPE_INFO = "addRecipeInformation"
     const val QUERY_FILL_INGREDIENTS = "fillIngredients"
+
+    // Room Database
+    const val DATABASE_NAME ="recipes_database"
+    const val RECIPES_TABLE_NAME = "recipes_table"
+
 }
