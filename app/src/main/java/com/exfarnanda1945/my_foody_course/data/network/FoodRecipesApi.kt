706763222a97d63a2ac67ee74ddebe5f74f4c26f
exfarnanda1945 <exfarnanda1945@gmail.com>
2022-12-15 22:01:52 +0700

    add: search recipes api

--- app/src/main/java/com/exfarnanda1945/my_foody_course/data/network/FoodRecipesApi.kt
+++ app/src/main/java/com/exfarnanda1945/my_foody_course/data/network/FoodRecipesApi.kt
@@ -8,6 +8,11 @@ import retrofit2.http.QueryMap
 interface FoodRecipesApi {
     @GET("/recipes/complexSearch")
     suspend fun getRecipes(
-        @QueryMap queries:Map<String,String>
-    ):Response<FoodRecipe>
+        @QueryMap queries: Map<String, String>
+    ): Response<FoodRecipe>
+
+    @GET("/recipes/complexSearch")
+    suspend fun searchRecipes(
+        @QueryMap queries: Map<String, String>
+    ): Response<FoodRecipe>
 }
