e4f892fa44ee85fea525376745de77b134661ef0
exfarnanda1945 <exfarnanda1945@gmail.com>
2022-12-15 22:27:14 +0700

    fix: nullable at result item model

--- app/src/main/java/com/exfarnanda1945/my_foody_course/model/ResultsItem.kt
+++ app/src/main/java/com/exfarnanda1945/my_foody_course/model/ResultsItem.kt
@@ -1,59 +1,60 @@
 package com.exfarnanda1945.my_foody_course.model
 
-import kotlinx.parcelize.Parcelize
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
 
 @Parcelize
 data class ResultsItem(
-	@field:SerializedName("sustainable")
-	val sustainable: Boolean,
+    @field:SerializedName("sustainable")
+    val sustainable: Boolean,
 
-	@field:SerializedName("glutenFree")
-	val glutenFree: Boolean,
+    @field:SerializedName("glutenFree")
+    val glutenFree: Boolean,
 
-	@field:SerializedName("title")
-	val title: String,
+    @field:SerializedName("title")
+    val title: String? = null,
 
-	@field:SerializedName("aggregateLikes")
-	val aggregateLikes: Int,
+    @field:SerializedName("aggregateLikes")
+    val aggregateLikes: Int? = 0,
 
-	@field:SerializedName("readyInMinutes")
-	val readyInMinutes: Int,
+    @field:SerializedName("readyInMinutes")
+    val readyInMinutes: Int? = 0,
 
-	@field:SerializedName("sourceUrl")
-	val sourceUrl: String,
+    @field:SerializedName("sourceUrl")
+    val sourceUrl: String? = null,
 
-	@field:SerializedName("dairyFree")
-	val dairyFree: Boolean,
+    @field:SerializedName("dairyFree")
+    val dairyFree: Boolean,
 
-	@field:SerializedName("vegetarian")
-	val vegetarian: Boolean,
+    @field:SerializedName("vegetarian")
+    val vegetarian: Boolean,
 
-	@field:SerializedName("id")
-	val id: Int,
+    @field:SerializedName("id")
+    val id: Int? = 0,
 
-	@field:SerializedName("summary")
-	val summary: String,
+    @field:SerializedName("summary")
+    val summary: String? = null,
 
-	@field:SerializedName("image")
-	val image: String,
+    @field:SerializedName("image")
+    val image: String? = null,
 
-	@field:SerializedName("veryHealthy")
-	val veryHealthy: Boolean,
+    @field:SerializedName("veryHealthy")
+    val veryHealthy: Boolean,
 
-	@field:SerializedName("vegan")
-	val vegan: Boolean,
+    @field:SerializedName("vegan")
+    val vegan: Boolean,
 
-	@field:SerializedName("cheap")
-	val cheap: Boolean,
+    @field:SerializedName("cheap")
+    val cheap: Boolean,
 
-	@field:SerializedName("extendedIngredients")
-	val extendedIngredients: List<ExtendedIngredientsItem>,
+    @field:SerializedName("extendedIngredients")
+    val extendedIngredients: @RawValue List<ExtendedIngredientsItem>,
 
-	@field:SerializedName("license")
-	val license: String,
+    @field:SerializedName("license")
+    val license: String? = null,
 
-	@field:SerializedName("sourceName")
-	val sourceName: String,
+    @field:SerializedName("sourceName")
+    val sourceName: String? = null,
 ) : Parcelable
