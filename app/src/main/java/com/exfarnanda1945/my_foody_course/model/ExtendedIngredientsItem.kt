c0c35ff84eb4a3826cb10bb1c1c673dfad5ef7e0
exfarnanda1945 <exfarnanda1945@gmail.com>
2022-12-15 22:02:19 +0700

    fix: ExtendedIngredientsItem nullable

--- app/src/main/java/com/exfarnanda1945/my_foody_course/model/ExtendedIngredientsItem.kt
+++ app/src/main/java/com/exfarnanda1945/my_foody_course/model/ExtendedIngredientsItem.kt
@@ -1,26 +1,26 @@
 package com.exfarnanda1945.my_foody_course.model
 
-import kotlinx.parcelize.Parcelize
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class ExtendedIngredientsItem(
-	@field:SerializedName("image")
-	val image: String,
+    @field:SerializedName("image")
+    val image: String? = null,
 
-	@field:SerializedName("amount")
-	val amount: Double,
+    @field:SerializedName("amount")
+    val amount: Double? = 0.0,
 
-	@field:SerializedName("unit")
-	val unit: String,
+    @field:SerializedName("unit")
+    val unit: String? = null,
 
-	@field:SerializedName("original")
-	val original: String,
+    @field:SerializedName("original")
+    val original: String? = null,
 
-	@field:SerializedName("name")
-	val name: String,
+    @field:SerializedName("name")
+    val name: String? = null,
 
-	@field:SerializedName("consistency")
-	val consistency: String
+    @field:SerializedName("consistency")
+    val consistency: String? = null
 ) : Parcelable
