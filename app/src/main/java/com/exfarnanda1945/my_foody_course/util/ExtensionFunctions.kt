3207369f66674289fc42f328b1f59f1d7143c99a
exfarnanda1945 <exfarnanda1945@gmail.com>
2022-12-15 22:01:36 +0700

    add: extension function

--- app/src/main/java/com/exfarnanda1945/my_foody_course/util/ExtensionFunctions.kt
+++ app/src/main/java/com/exfarnanda1945/my_foody_course/util/ExtensionFunctions.kt
@@ -1,8 +1,12 @@
 package com.exfarnanda1945.my_foody_course.util
 
+import android.os.Build.VERSION.SDK_INT
+import android.os.Bundle
+import android.os.Parcelable
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import java.util.*
 
 // to call observer once
 fun <T> LiveData<T>.observerOnce(lifecycleOwner: LifecycleOwner,observer: Observer<T>){
@@ -12,4 +16,20 @@ fun <T> LiveData<T>.observerOnce(lifecycleOwner: LifecycleOwner,observer: Observ
             observer.onChanged(t)
         }
     })
+}
+
+inline fun <reified T : Parcelable> Bundle.parcelable(key: String): T? = when {
+    SDK_INT >= 33 -> getParcelable(key, T::class.java)
+    else -> @Suppress("DEPRECATION") getParcelable(key) as? T
+}
+
+/**
+ * Replacement for Kotlin's deprecated `capitalize()` function.
+ */
+fun String.capitalized(): String {
+    return this.replaceFirstChar {
+        if (it.isLowerCase())
+            it.titlecase(Locale.getDefault())
+        else it.toString()
+    }
 }
