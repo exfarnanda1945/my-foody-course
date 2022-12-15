4f1e7e19af8e3ae7a4c5c91dda4ea2a6fe234ec8
exfarnanda1945 <exfarnanda1945@gmail.com>
2022-12-15 21:59:19 +0700

    fix: add more constant

--- app/src/main/java/com/exfarnanda1945/my_foody_course/util/Constants.kt
+++ app/src/main/java/com/exfarnanda1945/my_foody_course/util/Constants.kt
@@ -2,10 +2,12 @@ package com.exfarnanda1945.my_foody_course.util
 
 object Constants {
     // API
-    const val API_KEY ="eae6ab421f554d2a8f5ff823ec9467ea"
-    const val BASE_URL ="https://api.spoonacular.com"
+    const val API_KEY = "eae6ab421f554d2a8f5ff823ec9467ea"
+    const val BASE_URL = "https://api.spoonacular.com"
+    const val BASE_IMAGE_URL = "https://spoonacular.com/cdn/ingredients_100x100/"
 
     // Query Api
+    const val QUERY_SEARCH = "query"
     const val QUERY_NUMBER = "number"
     const val QUERY_API_KEY = "apiKey"
     const val QUERY_TYPE = "type"
@@ -14,7 +16,21 @@ object Constants {
     const val QUERY_FILL_INGREDIENTS = "fillIngredients"
 
     // Room Database
-    const val DATABASE_NAME ="recipes_database"
+    const val DATABASE_NAME = "recipes_database"
     const val RECIPES_TABLE_NAME = "recipes_table"
+
+    // Bottom Sheet and Preferences
+    const val DEFAULT_RECIPES_NUMBER = "50"
+    const val DEFAULT_MEAL_TYPE = "main course"
+    const val DEFAULT_DIET_TYPE = "gluten free"
+
+    const val PREFERENCES_NAME = "foody_preferences"
+    const val PREFERENCES_MEAL_TYPE = "mealType"
+    const val PREFERENCES_MEAL_TYPE_ID = "mealTypeId"
+    const val PREFERENCES_DIET_TYPE = "dietType"
+    const val PREFERENCES_DIET_TYPE_ID = "dietTypeId"
+    const val PREFERENCES_BACK_ONLINE = "backOnline"
+
+    const val RECIPE_BUNDLE_KEY = "recipeBundle"
 
 }
