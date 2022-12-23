5114fb1d5adabda2da70d51a84501386c3630968
exfarnanda1945 <exfarnanda1945@gmail.com>
2022-12-23 22:05:44 +0700

    fix: instruction fragment and layout

--- app/src/main/java/com/exfarnanda1945/my_foody_course/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/exfarnanda1945/my_foody_course/ui/fragments/instructions/InstructionsFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.exfarnanda1945.my_foody_course.databinding.FragmentInstructionsBinding
+import com.exfarnanda1945.my_foody_course.model.ResultsItem
+import com.exfarnanda1945.my_foody_course.util.Constants
+import com.exfarnanda1945.my_foody_course.util.parcelable
 
 
 class InstructionsFragment : Fragment() {
@@ -17,6 +21,18 @@ class InstructionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val resultBundle = args?.parcelable<ResultsItem>(Constants.RECIPE_BUNDLE_KEY)
+
+        binding.instructionWebView.apply {
+            webViewClient = object : WebViewClient() {}
+            resultBundle?.sourceUrl?.let {
+                loadUrl(it)
+            }
+
+        }
+
         return binding.root
     }
 
