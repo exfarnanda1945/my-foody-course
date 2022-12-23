795bf7ed1bd467a9c6c98b4ab38dc55f1aef8388
exfarnanda1945 <exfarnanda1945@gmail.com>
2022-12-23 21:38:45 +0700

    fix: add interceptor in network module

--- app/src/main/java/com/exfarnanda1945/my_foody_course/di/NetworkModule.kt
+++ app/src/main/java/com/exfarnanda1945/my_foody_course/di/NetworkModule.kt
@@ -1,11 +1,14 @@
 package com.exfarnanda1945.my_foody_course.di
 
 import com.exfarnanda1945.my_foody_course.data.network.FoodRecipesApi
+import com.exfarnanda1945.my_foody_course.util.Constants.API_KEY
 import com.exfarnanda1945.my_foody_course.util.Constants.BASE_URL
+import com.exfarnanda1945.my_foody_course.util.Constants.QUERY_API_KEY
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -15,6 +18,18 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    private fun provideInterceptor(): Interceptor = Interceptor { chain ->
+        val request = chain.request()
+        val url = request.url().newBuilder().addQueryParameter(
+            QUERY_API_KEY, API_KEY
+        ).build()
+
+        chain.proceed(
+            request.newBuilder().url(url).build()
+        )
+    }
+
     @Singleton
     @Provides
     fun provideConverter(): GsonConverterFactory = GsonConverterFactory.create()
@@ -24,6 +39,7 @@ object NetworkModule {
     fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
         .readTimeout(15, TimeUnit.SECONDS)
         .connectTimeout(15, TimeUnit.SECONDS)
+        .addInterceptor(provideInterceptor())
         .build()
 
     @Singleton
